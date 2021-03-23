d7a8b048fb96c6a3077e61c83a0e6d5a188a9fb2
Soham Aserkar <soham339@gmail.com>
2021-03-23 20:31:42 +0530

    CodeInPlace test

---
+++ DL/Mars_Weight.py
@@ -0,0 +1,6 @@
+def main():
+    """An earthling's weight on Mars is 37.8% of their weight on earth. Write a Python program that prompts the user
+    (an earthling) to enter their weight on earth and prints their calculated weight on Mars. """
+
+
+if __name__ == "__main__": main()
